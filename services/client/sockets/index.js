0ea314259daa5efb31f581ec836a5fb195361cd1
1Mill <erik.ekberg@outlook.com>
2020-07-05 20:20:50 -0700

    Update index.js

--- services/client/sockets/index.js
+++ services/client/sockets/index.js
@@ -1,1 +1,23 @@
-console.log('Hello world!');
+const ioMiddlewareWildcard = require('socketio-wildcard')();
+const { KAFKA_EVENTTYPE, subscribe } = require("@1mill/cloudevents");
+
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+io.use(ioMiddlewareWildcard);
+
+// subscribe({
+// 	handler: async() => {
+// 		console.log('Hello world!');
+// 	},
+// 	id: 'services.client.sockets',
+// 	subscribeEventType: KAFKA_EVENTTYPE,
+// 	subscribeTo: [],
+// 	types: ['hello-world.2020-07-05']
+// })
+
+const main = async() => {
+	const { getSecret } = require('./utility/getSecret');
+	const data = await getSecret({ path: './secrets/brokers.sops.json' });
+	console.log(data);
+};
+main();
