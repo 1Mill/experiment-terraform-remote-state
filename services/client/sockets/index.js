6315af802d5625d797e619a1264b7683bd162158
1Mill <erik.ekberg@outlook.com>
2020-07-06 14:52:46 -0700

    Tweek everything to connect to rapids

--- services/client/sockets/index.js
+++ services/client/sockets/index.js
@@ -1,23 +1,21 @@
 const ioMiddlewareWildcard = require('socketio-wildcard')();
-const { KAFKA_EVENTTYPE, subscribe } = require("@1mill/cloudevents");
+const { KAFKA_EVENTTYPE, subscribe } = require('@1mill/cloudevents');
 const { getSecret } = require('./utilities/getSecret');
 
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 io.use(ioMiddlewareWildcard);
 
-// subscribe({
-// 	handler: async() => {
-// 		console.log('Hello world!');
-// 	},
-// 	id: 'services.client.sockets',
-// 	subscribeEventType: KAFKA_EVENTTYPE,
-// 	subscribeTo: [],
-// 	types: ['hello-world.2020-07-05']
-// })
-
 const main = async() => {
-	const data = await getSecret({ path: './secrets/brokers.sops.json' });
-	console.log(data);
+	const rapidsUrls = await getSecret({ path: './secrets/brokers.sops.json' });
+	subscribe({
+		handler: async () => {
+			console.log('Hello world!');
+		},
+		id: 'services.client.sockets',
+		subscribeEventType: KAFKA_EVENTTYPE,
+		subscribeTo: rapidsUrls,
+		types: ['hello-world.2020-07-05']
+	});
 };
 main();
