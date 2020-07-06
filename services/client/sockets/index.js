f4d83aacb96eae1866ae88f2764dc5b3bbadf893
1Mill <erik.ekberg@outlook.com>
2020-07-06 14:59:23 -0700

    Refactor how rapids are input into application

--- services/client/sockets/index.js
+++ services/client/sockets/index.js
@@ -7,14 +7,13 @@ const io = require('socket.io')(server);
 io.use(ioMiddlewareWildcard);
 
 const main = async() => {
-	const rapidsUrls = await getSecret({ path: './secrets/brokers.sops.json' });
 	subscribe({
 		handler: async () => {
 			console.log('Hello world!');
 		},
 		id: 'services.client.sockets',
 		subscribeEventType: KAFKA_EVENTTYPE,
-		subscribeTo: rapidsUrls,
+		subscribeTo: process.env.RAPIDS_URLS.split(','),
 		types: ['hello-world.2020-07-05']
 	});
 };
