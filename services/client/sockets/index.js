e4a49c080519214bd971a7e60cfbffce7227c488
1Mill <erik.ekberg@outlook.com>
2020-07-06 21:33:15 -0700

    Add example of supporting a dead letter exchange

--- services/client/sockets/index.js
+++ services/client/sockets/index.js
@@ -22,15 +22,41 @@ const broker = createBroker({
 
 subscribe({
 	broker,
-	handler: async({ isEnriched }) => {
-		if (isEnriched) {
-			console.log('Enriched message was got');
-		} else {
-			console.log('Sending enriched message');
-			return true;
+	handler: async({ isEnriched, cloudevent }) => {
+		try {
+			if ([true, false, false, false, false][Math.floor(Math.random() * 5)]) {
+				throw new Error('Some kind of error happened');
+			}
+			if (isEnriched) {
+				console.log('Enriched message was got');
+			} else {
+				console.log('Sending enriched message');
+				return true;
+			}
+		} catch (err) {
+			console.error(err);
+			publish({
+				broker,
+				cloudevent: {
+					...cloudevent,
+					dlx: 'dead-letter',
+				},
+			});
 		}
 	},
 	types: ['hello-world.2020-07-06'],
+});
+
+subscribe({
+	broker: createBroker({
+		eventType: KAFKA_EVENTTYPE,
+		id: 'dead-letter-example',
+		urls: RAPIDS_URLS,
+	}),
+	handler: async () => {
+		console.log('Dead letter message was got');
+	},
+	types: ['dead-letter'],
 });
 
 setInterval(async() => {
@@ -42,4 +68,4 @@ setInterval(async() => {
 		broker,
 		cloudevent,
 	});
-}, 3000);
+}, 5000);
