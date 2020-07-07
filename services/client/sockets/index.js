2847c1b3837c53fd58371f5ae820178e5b858ed5
1Mill <erik.ekberg@outlook.com>
2020-07-06 20:39:47 -0700

    Use new cloudevent brokers

--- services/client/sockets/index.js
+++ services/client/sockets/index.js
@@ -1,20 +1,20 @@
 const ioMiddlewareWildcard = require('socketio-wildcard')();
-const { KAFKA_EVENTTYPE, subscribe } = require('@1mill/cloudevents');
-const { getSecret } = require('./utilities/getSecret');
+const {
+	KAFKA_EVENTTYPE,
+	createBroker,
+} = require('@1mill/cloudevents');
 
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 io.use(ioMiddlewareWildcard);
 
-const main = async() => {
-	subscribe({
-		handler: async () => {
-			console.log('Hello world!');
-		},
-		id: 'services.client.sockets',
-		subscribeEventType: KAFKA_EVENTTYPE,
-		subscribeTo: process.env.RAPIDS_URLS.split(','),
-		types: ['hello-world.2020-07-05']
-	});
-};
-main();
+const ID = 'services.client.sockets';
+const RAPIDS_URLS = process.env.RAPIDS_URLS.split(',');
+
+const broker = createBroker({
+	eventType: KAFKA_EVENTTYPE,
+	id: ID,
+	urls: RAPIDS_URLS,
+});
+
+console.log(broker);
