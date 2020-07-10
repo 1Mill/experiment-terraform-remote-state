674af0dcb6740abd22a2eecbbed1cb70515d9659
1Mill <erik.ekberg@outlook.com>
2020-07-10 08:49:59 -0700

    Abstract node with heroku integration into platform module

--- services/client/main.tf
+++ services/client/main.tf
@@ -14,43 +14,11 @@ terraform {
 	}
 }
 
-// Required providers
-module "heroku" {
-	source = "./modules/secrets"
-	file_name = "heroku.sops.json"
-}
-provider "heroku" {
-	api_key = module.heroku.json.APIKEY
-	email = module.heroku.json.EMAIL
-	version = "~> 2.5"
-}
-
 // Create, build, and release application
-resource "heroku_app" "default" {
-	name = "services-client-ui"
-	region = "us"
-}
-resource "heroku_app_config_association" "default" {
-	app_id = heroku_app.default.id
-	vars = {
-		PROJECT_PATH = "services/client/ui"
-	}
-}
-resource "heroku_build" "default" {
-	app = heroku_app.default.name
-	buildpacks = [
-		"https://github.com/timanovsky/subdir-heroku-buildpack",
-		"https://github.com/heroku/heroku-buildpack-nodejs",
-	]
-	source = {
-		url = "https://github.com/1Mill/experiment-terraform-remote-state/archive/v0.0.6.tar.gz"
-		version = "v0.0.6"
-	}
-}
-resource "heroku_formation" "default" {
-	app = heroku_app.default.id
-	depends_on = [ heroku_build.default ]
-	quantity = 1
-	size = "free"
-	type = "web"
+module "client_ui" {
+	source = "./modules/platforms/node"
+
+	application_name = "services-client-ui"
+	application_project_path = "services/client/ui"
+	application_version = "v0.0.6"
 }
---
+++ services/client/modules/platforms/node/main.tf
@@ -0,0 +1,51 @@
+// Module inputs
+variable "application_name" {
+	type = string
+}
+variable "application_project_path" {
+	type = string
+}
+variable "application_version" {
+	type = string
+}
+
+// Required providers
+module "heroku" {
+	source = "../../secrets"
+	file_name = "heroku.sops.json"
+}
+provider "heroku" {
+	api_key = module.heroku.json.APIKEY
+	email = module.heroku.json.EMAIL
+	version = "~> 2.5"
+}
+
+// Create, build, and release application
+resource "heroku_app" "default" {
+	name = var.application_name
+	region = "us"
+}
+resource "heroku_app_config_association" "default" {
+	app_id = heroku_app.default.id
+	vars = {
+		PROJECT_PATH = var.application_project_path
+	}
+}
+resource "heroku_build" "default" {
+	app = heroku_app.default.name
+	buildpacks = [
+		"https://github.com/timanovsky/subdir-heroku-buildpack",
+		"https://github.com/heroku/heroku-buildpack-nodejs",
+	]
+	source = {
+		url = "https://github.com/1Mill/experiment-terraform-remote-state/archive/${var.application_version}.tar.gz"
+		version = var.application_version
+	}
+}
+resource "heroku_formation" "default" {
+	app = heroku_app.default.id
+	depends_on = [ heroku_build.default ]
+	quantity = 1
+	size = "free"
+	type = "web"
+}
