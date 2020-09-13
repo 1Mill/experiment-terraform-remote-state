a28c2be4b6e52ac91ca93e38db83a381630990cb
1Mill <erik.ekberg@outlook.com>
2020-09-13 13:00:04 -0700

    Update index.js

--- services/modify-string/index.js
+++ services/modify-string/index.js
@@ -2,8 +2,17 @@ const {
 	v2: { createEventStream, enrich }
 } = require('@1mill/cloudevents')
 
+const ID = 'services.modify-string'
 const rapids = createEventStream({
-	id: 'services.modify-string',
+	id: ID,
+	mechanism: process.env.RAPIDS_MECHANISM,
+	password: process.env.RAPIDS_PASSWORD,
+	protocal: process.env.RAPIDS_PROTOCAL,
+	urls: (process.env.RAPIDS_URLS || '').split(','),
+	username: process.env.RAPIDS_USERNAME,
+})
+const river = createEventStream({
+	id: ID,
 	mechanism: process.env.RAPIDS_MECHANISM,
 	password: process.env.RAPIDS_PASSWORD,
 	protocal: process.env.RAPIDS_PROTOCAL,
@@ -11,15 +20,18 @@ const rapids = createEventStream({
 	username: process.env.RAPIDS_USERNAME,
 })
 
-rapids.listen({
+river.listen({
 	handler: async ({ cloudevent, data, isEnriched }) => {
 		try {
+			// If already enriched, do nothing
 			if (isEnriched) { return }
 
+			// Perform buisness logic
 			const string = data || ''
 			const numbers = string.match(/[0-9 , \.]+/g) || []
 			const enrichment = numbers.join('')
 
+			// Emit enriched cloudevent back to rapids
 			await rapids.emit({
 				cloudevent: enrich({ cloudevent, enrichment })
 			})
